3e36265fed88ea25fcbcb7721dea22e4ff1d290e
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2024-01-19 23:24:56 +0100

    Create Esercizio3.js

---
+++ Esercizio3.js
@@ -0,0 +1,3 @@
+// Hello Component: Puoi integrare il componente Hello più volte all'interno del componente App. Ciascuna istanza del componente Hello funzionerà in modo indipendente dalle altre.
+
+// Message Component: Puoi collocare direttamente il componente Message all'interno del componente App. Ciò causerà la visualizzazione del messaggio "What a beautiful day!" ogni volta che il componente Message viene utilizzato nel tuo componente App.
